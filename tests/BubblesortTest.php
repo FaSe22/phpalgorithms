9bebc8fa20715c939f592a34c3ea0b07d1a4ae70
GitHub Action <action@github.com>
2023-09-19 20:56:13 +0000

    Apply PHP-CS-Fixer

--- tests/BubblesortTest.php
+++ tests/BubblesortTest.php
@@ -28,26 +28,25 @@ final class BubblesortTest extends TestCase
         [[9,8,7,1,2,3,4,5,6]],
         [[9,1,8,2,7,3,6,4,5]],
         [[1,9,2,8,3,7,4,6,5]],
-        [[6,4,1,8,3,9,2,5,7]]
+        [[6,4,1,8,3,9,2,5,7]],
     ];
     }
-  
-    #[Test]
-  public function it_should_sort_single_element_array()
-  {
-    $this->assertEquals([4], Bubblesort::sort([4]));
-  }
 
     #[Test]
-  public function test_empty_array()
-  {
-    $this->assertEquals([], Bubblesort::sort([]));
-  }
+    public function it_should_sort_single_element_array()
+    {
+        $this->assertEquals([4], Bubblesort::sort([4]));
+    }
 
-  #[Test]
-  public function it_should_sort_non_consecutive_numbers_correctly()
-  {
-    $this->assertEquals([2,5,6,8,9], Bubblesort::sort([5,9,6,2,8]));
-  }
+    #[Test]
+    public function test_empty_array()
+    {
+        $this->assertEquals([], Bubblesort::sort([]));
+    }
 
+    #[Test]
+    public function it_should_sort_non_consecutive_numbers_correctly()
+    {
+        $this->assertEquals([2,5,6,8,9], Bubblesort::sort([5,9,6,2,8]));
+    }
 }
