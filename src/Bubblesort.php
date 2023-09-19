ec4f0ae8198314b9d74f4b60c1f6e9502fbed232
Sebastian <fase22290@icloud.com>
2023-09-19 22:51:59 +0200

    update Bubblesort Test

--- src/Bubblesort.php
+++ src/Bubblesort.php
@@ -4,20 +4,20 @@ namespace Src;
 
 class Bubblesort
 {
-    
-    /**
-    * @param array $arr
-    * @return array
-    */  
     public static function sort(array $arr): array
     {
+        /**
+ *
+ * @var int[] $arr
+ */
+        $arr = $arr;
         for ($j = count($arr) - 1; $j > 0; $j--) {
             for($i = 0; $i < $j; $i++) {
                 if($arr[$i] > $arr[$i + 1]) {
-                    [$arr[$i], $arr[$i + 1]] = [$arr[$i + 1], $arr[$i]];
+                    [$arr[$i],  $arr[$i + 1]] = [$arr[$i + 1], $arr[$i]];
                 }
             }
         }
-      return $arr;
+        return $arr;
     }
 }
