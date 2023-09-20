4f684c934bb9a08a4450d1230857371cf8da14f5
dimitrijjedich <dimitrij@startup-werk.de>
2023-09-20 13:45:40 +0200

    add Node class to BinaryTree.php

--- src/BinaryTree.php
+++ src/BinaryTree.php
@@ -13,3 +13,44 @@ class BinaryTree
         return $arr;
     }
 }
+
+class Node
+{
+    private Node|null $parent;
+    private Node|null $right;
+    private Node|null $left;
+
+    public function __construct(Node $node = null)
+    {
+        $this->parent = $node;
+        $this->left = null;
+        $this->right = null;
+    }
+
+    public function parent(): Node
+    {
+        return $this->parent;
+    }
+
+    public function left(): Node
+    {
+        return $this->left;
+    }
+
+    public function right(): Node
+    {
+        return $this->right;
+    }
+
+    public function setLeft(): Node
+    {
+        $newNode = new Node($this);
+        return $this->left = $newNode;
+    }
+
+    public function setRight(): Node
+    {
+        $newNode = new Node($this);
+        return $this->right = $newNode;
+    }
+}
