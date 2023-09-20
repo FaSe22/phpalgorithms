91fa292e2366fe0109c8be3f855c661b71724458
Dimitrij Jedich <dimitrij@startup-werk.de>
2023-09-20 22:21:03 +0200

    fix switch of elements in last implementation of Bubblesort

--- src/Bubblesort.php
+++ src/Bubblesort.php
@@ -72,7 +72,7 @@ class Bubblesort
             $check = false;
             for ($i=0; $i < count($arr)-1; $i++) {
                 if ($arr[$i] > $arr[$i+1]) {
-                    $tmp = $arr[$i+1];
+                    $tmp = $arr[$i];
                     $arr[$i] = $arr[$i+1];
                     $arr[$i+1] = $tmp;
                     $check = true;
