f3d21989e84d261f14fd9d0d9881c57840adb1c3
Dimitrij Jedich <dimitrij@startup-werk.de>
2023-10-18 22:51:39 +0200

    add second variant of sort to Insertionsort.php

--- src/Insertionsort.php
+++ src/Insertionsort.php
@@ -33,4 +33,26 @@ class Insertionsort
         }
         return $arr;
     }
+
+    private static function variant2(array $arr): array
+    {
+        // start from the first array and iterate over all
+        for ($i = 0; $i < count($arr); $i++) {
+            // assume the first element is the smallest
+            $index = $i;
+            // loop over the rest of the array
+            for ($j = $i; $j < count($arr); $j++) {
+                // check if the current element is smaller
+                if ($arr[$j]<$arr[$index]) {
+                    // replace the saved index
+                    $index = $j;
+                }
+            }
+            // We now know the smallest number is at $index and can replace it with the starting position
+            $safe = $arr[$index];
+            $arr[$index] = $arr[$i];
+            $arr[$i] = $safe;
+        }
+        return $arr;
+    }
 }
