182c7ffb709e7dda6c0e0c6439b7e5965a6fa429
dimitrijjedich <dimitrij@startup-werk.de>
2023-10-23 20:11:17 +0200

    extract Classes into own files

--- src/BinaryTree.php
+++ src/BinaryTree.php
@@ -2,6 +2,8 @@
 
 namespace Src;
 
+use Src\helper\Tree;
+
 class BinaryTree
 {
     /**
@@ -15,102 +17,5 @@ class BinaryTree
             $tree->put($arr[$i]);
         }
         return $tree->root()->flatten();
-    }
-}
-
-class Tree
-{
-    private Node|null $root = null;
-
-    public function __construct($value)
-    {
-        $this->root = new Node($value);
-    }
-
-    public function root(): Node
-    {
-        return $this->root;
-    }
-
-    public function put($value, Node $node=null): void
-    {
-        if ($node == null) {
-            $this->put($value, $this->root);
-            return;
-        }
-        if ($node->value() < $value) {
-            if ($node->right() == null) {
-                $node->setRight($value);
-            } else {
-                $this->put($value, $node->right());
-            }
-        } else {
-            if ($node->left() == null) {
-                $node->setLeft($value);
-            } else {
-                $this->put($value, $node->left());
-            }
-        }
-    }
-}
-
-class Node
-{
-    private Node|null $parent;
-    private Node|null $right;
-    private Node|null $left;
-    private Int $value;
-
-    public function __construct($value, Node $node = null)
-    {
-        $this->parent = $node;
-        $this->value = $value;
-        $this->left = null;
-        $this->right = null;
-    }
-
-    public function parent(): Node|null
-    {
-        return $this->parent;
-    }
-
-    public function value(): Int
-    {
-        return $this->value;
-    }
-
-    public function left(): Node|null
-    {
-        return $this->left;
-    }
-
-    public function right(): Node|null
-    {
-        return $this->right;
-    }
-
-    public function setLeft($value): Node
-    {
-        $newNode = new Node($value, $this);
-        return $this->left = $newNode;
-    }
-
-    public function setRight($value): Node
-    {
-        $newNode = new Node($value, $this);
-        return $this->right = $newNode;
-    }
-
-    public function flatten(): array
-    {
-        $left = [];
-        $right = [];
-        if ($this->left() != null) {
-            $left = $this->left()->flatten();
-        }
-        if ($this->right != null) {
-            $right =  $this->right()->flatten();
-        }
-        return array_merge($left, [$this->value], $right);
     }
 }
---
+++ src/helper/Node.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Src\helper;
+
+class Node
+{
+    private Node|null $parent;
+    private Node|null $right;
+    private Node|null $left;
+    private Int $value;
+
+    public function __construct($value, Node $node = null)
+    {
+        $this->parent = $node;
+        $this->value = $value;
+        $this->left = null;
+        $this->right = null;
+    }
+
+    public function parent(): Node|null
+    {
+        return $this->parent;
+    }
+
+    public function value(): Int
+    {
+        return $this->value;
+    }
+
+    public function left(): Node|null
+    {
+        return $this->left;
+    }
+
+    public function right(): Node|null
+    {
+        return $this->right;
+    }
+
+    public function setLeft($value): Node
+    {
+        $newNode = new Node($value, $this);
+        return $this->left = $newNode;
+    }
+
+    public function setRight($value): Node
+    {
+        $newNode = new Node($value, $this);
+        return $this->right = $newNode;
+    }
+
+    public function flatten(): array
+    {
+        $left = [];
+        $right = [];
+        if ($this->left() != null) {
+            $left = $this->left()->flatten();
+        }
+        if ($this->right != null) {
+            $right =  $this->right()->flatten();
+        }
+        return array_merge($left, [$this->value], $right);
+    }
+}
