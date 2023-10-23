5808c3edc15cfb8485bc45281a2a027968006607
dimitrijjedich <dimitrij@startup-werk.de>
2023-10-23 18:07:06 +0200

    add Insertionsort tests for variant2

--- tests/InsertionsortTest.php
+++ tests/InsertionsortTest.php
@@ -14,6 +14,7 @@ final class InsertionsortTest extends TestCase
     public function it_should_return_the_sorted_array($arr)
     {
         $this->assertEquals([1,2,3,4,5,6,7,8,9], InsertionSort::sort($arr));
+        $this->assertEquals([1,2,3,4,5,6,7,8,9], InsertionSort::sort($arr, "variant2"));
     }
 
     /*
@@ -36,18 +37,21 @@ final class InsertionsortTest extends TestCase
     public function it_should_sort_single_element_array()
     {
         $this->assertEquals([4], InsertionSort::sort([4]));
+        $this->assertEquals([4], InsertionSort::sort([4], "variant2"));
     }
 
     #[Test]
     public function test_empty_array()
     {
         $this->assertEquals([], InsertionSort::sort([]));
+        $this->assertEquals([], InsertionSort::sort([], "variant2"));
     }
 
     #[Test]
     public function it_should_sort_non_consecutive_numbers_correctly()
     {
         $this->assertEquals([2,5,6,8,9], InsertionSort::sort([5,9,6,2,8]));
+        $this->assertEquals([2,5,6,8,9], InsertionSort::sort([5,9,6,2,8], "variant2"));
     }
 
     #[Test]
@@ -56,5 +60,6 @@ final class InsertionsortTest extends TestCase
         $random = range(1, 1000);
         shuffle($random);
         $this->assertEquals(range(1, 1000), InsertionSort::sort($random));
+        $this->assertEquals(range(1, 1000), InsertionSort::sort($random, "variant2"));
     }
 }
