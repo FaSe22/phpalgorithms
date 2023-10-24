c7dee1fe50c1cf839d0f141b01fde927d81b7e29
dimitrijjedich <dimitrij@startup-werk.de>
2023-10-24 10:21:48 +0200

    add QuickSort implementation

--- src/QuickSort.php
+++ src/QuickSort.php
@@ -4,5 +4,28 @@ namespace Src;
 
 class QuickSort
 {
-
+    /**
+     * @param array<int, float> $arr
+     * @return array<int, float>
+     */
+    public static function sort(array $arr): array
+    {
+        if (empty($arr)) {
+            return [];
+        }
+        $pivot = $arr[0];
+        $right = [];
+        $left = [];
+        for ($i= 1; $i < count($arr); $i++) {
+            if ($arr[$i]<$pivot) {
+                $right[] = $arr[$i];
+            } else {
+                $left[] = $arr[$i];
+            }
+        }
+        $right = self::sort($right);
+        $right[] = $pivot;
+        $left = self::sort($left);
+        return array_merge($right, $left);
+    }
 }
