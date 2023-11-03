56d18c747bfdb0ffda5fa1748ada92fd136fefc7
dimitrijjedich <dimitrij@startup-werk.de>
2023-11-03 17:00:02 +0100

    remove unnecessary variable

--- src/MergeSort.php
+++ src/MergeSort.php
@@ -15,9 +15,8 @@ class MergeSort
         }
         $middle = (int)(count($arr) / 2);
         $left = array_splice($arr, 0, $middle);
-        $right = $arr;
         $left = self::sort($left);
-        $right = self::sort($right);
+        $right = self::sort($arr);
         return self::merge($left, $right);
     }
 
