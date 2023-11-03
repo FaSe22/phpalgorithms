c278f588496f5a0e0963b14f5a021a75229b7b41
dimitrijjedich <dimitrij@startup-werk.de>
2023-11-03 16:06:34 +0100

    add implementation for splitting and recursive calling

--- src/MergeSort.php
+++ src/MergeSort.php
@@ -13,6 +13,11 @@ class MergeSort
         if (empty($arr)) {
             return [];
         }
-        return $arr;
+        $middle = (int)(count($arr)/2);
+        $left = array_splice($arr, 0, $middle);
+        $right = $arr;
+        $left = self::sort($left);
+        $right = self::sort($right);
+        return self::merge($left, $right);
     }
 }
