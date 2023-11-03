e9ee2e501494a17a981c79196ca15a64d15c4b6b
dimitrijjedich <dimitrij@startup-werk.de>
2023-11-03 14:58:05 +0100

    add sort method to MergeSort class

--- src/MergeSort.php
+++ src/MergeSort.php
@@ -4,4 +4,12 @@ namespace Src;
 
 class MergeSort
 {
+    /**
+     * @param array<int, float> $arr
+     * @return array<int, float>
+     */
+    public static function sort(array $arr): array
+    {
+        return $arr;
+    }
 }
