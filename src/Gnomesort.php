80a002016cb9bda1a630f8572109dc3e03fb56c6
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-15 19:50:32 +0100

    formatting

--- src/Gnomesort.php
+++ src/Gnomesort.php
@@ -11,11 +11,11 @@ class Gnomesort
     public static function sort(array $arr): array
     {
         $i = 0;
-        while ($i < count($arr)-1) {
+        while ($i < count($arr) - 1) {
             if ($arr[$i] > $arr[$i + 1]) {
                 $tmp = $arr[$i];
-                $arr[$i] = $arr[$i+1];
-                $arr[$i+1] = $tmp;
+                $arr[$i] = $arr[$i + 1];
+                $arr[$i + 1] = $tmp;
                 $i = max(0, $i - 1);
             } else {
                 $i = $i + 1;
