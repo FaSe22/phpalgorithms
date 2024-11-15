385b28737820592b6e36b9553304ae51f9126ba7
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-15 19:47:33 +0100

    add basic implementation

---
+++ src/Gnomesort.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Src;
+
+class Gnomesort
+{
+    /**
+     * @param array $arr
+     * @return array
+     */
+    public static function sort(array $arr): array
+    {
+        $i = 0;
+        while ($i < count($arr)-1) {
+            if ($arr[$i] > $arr[$i + 1]) {
+                $tmp = $arr[$i];
+                $arr[$i] = $arr[$i+1];
+                $arr[$i+1] = $tmp;
+                $i = $i == 0 ? 0 : $i-1;
+            } else {
+                $i = $i + 1;
+            }
+        }
+        return $arr;
+    }
+}
