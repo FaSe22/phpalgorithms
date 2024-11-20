f2b137d5c64aa94ad37194b3b8df97b8f8fc9f03
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-20 15:14:44 +0100

    update PHPDocs

--- tests/GnomesortTest.php
+++ tests/GnomesortTest.php
@@ -11,24 +11,27 @@ final class GnomesortTest extends TestCase
 {
     #[Test]
     #[DataProvider('arrayProvider')]
+    /**
+     * @param array<int> $arr
+     */
     public function it_should_return_the_sorted_array(array $arr): void
     {
         $this->assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 9], Gnomesort::sort($arr));
     }
 
     /**
-     * @return array
+     * @return array<array<int>>
      */
     public static function arrayProvider(): array
     {
         return [
-            [[1, 2, 3, 4, 5, 6, 7, 8, 9]],
-            [[9, 8, 7, 6, 5, 4, 3, 2, 1]],
-            [[1, 2, 3, 9, 8, 7, 6, 5, 4]],
-            [[9, 8, 7, 1, 2, 3, 4, 5, 6]],
-            [[9, 1, 8, 2, 7, 3, 6, 4, 5]],
-            [[1, 9, 2, 8, 3, 7, 4, 6, 5]],
-            [[6, 4, 1, 8, 3, 9, 2, 5, 7]],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [9, 8, 7, 6, 5, 4, 3, 2, 1],
+            [1, 2, 3, 9, 8, 7, 6, 5, 4],
+            [9, 8, 7, 1, 2, 3, 4, 5, 6],
+            [9, 1, 8, 2, 7, 3, 6, 4, 5],
+            [1, 9, 2, 8, 3, 7, 4, 6, 5],
+            [6, 4, 1, 8, 3, 9, 2, 5, 7],
         ];
     }
 
