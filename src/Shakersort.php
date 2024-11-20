063db02de5a37e327df69259b2143e083bcb1ce0
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-20 22:00:43 +0100

    add Shakersort to project

---
+++ src/Shakersort.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Src;
+
+class Shakersort
+{
+    /**
+     * @param array<int> $arr
+     * @return array<int>
+     */
+    public static function sort(array $arr): array
+    {
+        return $arr;
+    }
+}
