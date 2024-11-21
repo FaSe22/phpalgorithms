de3606a5896f838f192f4bec2476b443f66eb663
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-21 10:59:06 +0100

    formatting

--- tests/InsertionsortTest.php
+++ tests/InsertionsortTest.php
@@ -17,8 +17,8 @@ final class InsertionsortTest extends TestCase
     #[DataProvider('arrayProvider')]
     public function it_should_return_the_sorted_array(array $arr): void
     {
-        $this->assertEquals([1,2,3,4,5,6,7,8,9], InsertionSort::sort($arr));
-        $this->assertEquals([1,2,3,4,5,6,7,8,9], InsertionSort::sort($arr, "variant2"));
+        $this->assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 9], InsertionSort::sort($arr));
+        $this->assertEquals([1, 2, 3, 4, 5, 6, 7, 8, 9], InsertionSort::sort($arr, "variant2"));
     }
 
     /**
@@ -27,14 +27,14 @@ final class InsertionsortTest extends TestCase
     public static function arrayProvider(): array
     {
         return [
-        [[1,2,3,4,5,6,7,8,9]],
-        [[9,8,7,6,5,4,3,2,1]],
-        [[1,2,3,9,8,7,6,5,4]],
-        [[9,8,7,1,2,3,4,5,6]],
-        [[9,1,8,2,7,3,6,4,5]],
-        [[1,9,2,8,3,7,4,6,5]],
-        [[6,4,1,8,3,9,2,5,7]],
-    ];
+            [[1, 2, 3, 4, 5, 6, 7, 8, 9]],
+            [[9, 8, 7, 6, 5, 4, 3, 2, 1]],
+            [[1, 2, 3, 9, 8, 7, 6, 5, 4]],
+            [[9, 8, 7, 1, 2, 3, 4, 5, 6]],
+            [[9, 1, 8, 2, 7, 3, 6, 4, 5]],
+            [[1, 9, 2, 8, 3, 7, 4, 6, 5]],
+            [[6, 4, 1, 8, 3, 9, 2, 5, 7]],
+        ];
     }
 
     #[Test]
@@ -54,8 +54,8 @@ final class InsertionsortTest extends TestCase
     #[Test]
     public function it_should_sort_non_consecutive_numbers_correctly(): void
     {
-        $this->assertEquals([2,5,6,8,9], InsertionSort::sort([5,9,6,2,8]));
-        $this->assertEquals([2,5,6,8,9], InsertionSort::sort([5,9,6,2,8], "variant2"));
+        $this->assertEquals([2, 5, 6, 8, 9], InsertionSort::sort([5, 9, 6, 2, 8]));
+        $this->assertEquals([2, 5, 6, 8, 9], InsertionSort::sort([5, 9, 6, 2, 8], "variant2"));
     }
 
     #[Test]
