76936ea82963509a09db1ac0736d45bb43d13211
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-21 10:48:41 +0100

    add typing and return types to test methods

--- tests/BinaryTreeTest.php
+++ tests/BinaryTreeTest.php
@@ -11,7 +11,7 @@ final class BinaryTreeTest extends TestCase
 {
     #[Test]
     #[DataProvider('arrayProvider')]
-    public function it_should_return_the_sorted_array($arr)
+    public function it_should_return_the_sorted_array(array $arr): void
     {
         $this->assertEquals([1,2,3,4,5,6,7,8,9], BinaryTree::sort($arr));
     }
@@ -33,25 +33,25 @@ final class BinaryTreeTest extends TestCase
     }
 
     #[Test]
-    public function it_should_sort_single_element_array()
+    public function it_should_sort_single_element_array(): void
     {
         $this->assertEquals([4], BinaryTree::sort([4]));
     }
 
     #[Test]
-    public function test_empty_array()
+    public function test_empty_array(): void
     {
         $this->assertEquals([], BinaryTree::sort([]));
     }
 
     #[Test]
-    public function it_should_sort_non_consecutive_numbers_correctly()
+    public function it_should_sort_non_consecutive_numbers_correctly(): void
     {
         $this->assertEquals([2,5,6,8,9], BinaryTree::sort([5,9,6,2,8]));
     }
 
     #[Test]
-    public function it_can_sort_array_with_1000_elements()
+    public function it_can_sort_array_with_1000_elements(): void
     {
         $random = range(1, 1000);
         shuffle($random);
