121ddb7a7846369298c3f558d4211bce85ff22c2
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-22 23:05:10 +0100

    replace static array in tests and shuffled range with CustomDataProvider helper

--- tests/BubblesortTest.php
+++ tests/BubblesortTest.php
@@ -6,6 +6,7 @@ use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 use Src\Bubblesort;
+use Src\helper\CustomDataProvider;
 
 final class BubblesortTest extends TestCase
 {
@@ -28,15 +29,7 @@ final class BubblesortTest extends TestCase
     */
     public static function arrayProvider(): array
     {
-        return [
-        [[1,2,3,4,5,6,7,8,9]],
-        [[9,8,7,6,5,4,3,2,1]],
-        [[1,2,3,9,8,7,6,5,4]],
-        [[9,8,7,1,2,3,4,5,6]],
-        [[9,1,8,2,7,3,6,4,5]],
-        [[1,9,2,8,3,7,4,6,5]],
-        [[6,4,1,8,3,9,2,5,7]],
-    ];
+        return CustomDataProvider::oneToNine();
     }
 
     /*
--- tests/MergeSortTest.php
+++ tests/MergeSortTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
+use Src\helper\CustomDataProvider;
 use Src\MergeSort;
 
 final class MergeSortTest extends TestCase
@@ -21,15 +22,7 @@ final class MergeSortTest extends TestCase
     */
     public static function arrayProvider(): array
     {
-        return [
-        [[1,2,3,4,5,6,7,8,9]],
-        [[9,8,7,6,5,4,3,2,1]],
-        [[1,2,3,9,8,7,6,5,4]],
-        [[9,8,7,1,2,3,4,5,6]],
-        [[9,1,8,2,7,3,6,4,5]],
-        [[1,9,2,8,3,7,4,6,5]],
-        [[6,4,1,8,3,9,2,5,7]],
-    ];
+        return CustomDataProvider::oneToNine();
     }
 
     #[Test]
@@ -53,8 +46,7 @@ final class MergeSortTest extends TestCase
     #[Test]
     public function it_can_sort_array_with_1000_elements()
     {
-        $random = range(1, 1000);
-        shuffle($random);
-        $this->assertEquals(range(1, 1000), MergeSort::sort($random));
+        $random = CustomDataProvider::oneToNineNineNine()[0][0];
+        $this->assertEquals(range(1, 999), MergeSort::sort($random));
     }
 }
