ed7913b8871147f348c5ff272eccd30d81f08da3
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-23 20:54:06 +0100

    add implementation

--- src/SelectionSort.php
+++ src/SelectionSort.php
@@ -10,6 +10,18 @@ class SelectionSort
      */
     public static function sort(array $arr): array
     {
+        $length = count($arr);
+        for ($i = 0; $i < $length; $i++) {
+            $smallest = ['index' => $i, 'value' => $arr[$i]];
+            for ($j = $i + 1; $j < $length; $j++) {
+                if ($arr[$j] < $smallest['value']) {
+                    $smallest = ['index' => $j, 'value' => $arr[$j]];
+                }
+            }
+            if ($smallest['index'] != $i) {
+                [$arr[$smallest['index']], $arr[$i]] = [$arr[$i], $arr[$smallest['index']]];
+            }
+        }
         return $arr;
     }
 }
