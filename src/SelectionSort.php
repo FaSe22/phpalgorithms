cc42310c7616f639d4aa2e9f7225150c9125e0c0
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-23 20:40:00 +0100

    initial commit

---
+++ src/SelectionSort.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Src;
+
+class SelectionSort
+{
+    /**
+     * @param array<int> $arr
+     * @return array<int>
+     */
+    public static function sort(array $arr): array
+    {
+        return $arr;
+    }
+}
