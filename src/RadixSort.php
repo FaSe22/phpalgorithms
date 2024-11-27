60b41a7b7706cc661878c894b96eedc5f3caef04
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-27 23:24:50 +0100

    formatting

--- src/RadixSort.php
+++ src/RadixSort.php
@@ -14,11 +14,11 @@ class RadixSort
     {
         $n = count($arr);
         $partitions = [];
-        for($i = 0; $i < $n; $i++) {
-            $partitions[$arr[$i]%10][] = $arr[$i];
+        for ($i = 0; $i < $n; $i++) {
+            $partitions[$arr[$i] % 10][] = $arr[$i];
         }
         $arr = [];
-        for($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < 10; $i++) {
             $arr = array_merge($arr, $partitions[$i] ?? []);
         }
         return $arr;
