8d6c7a0a2cbfc560f749d6ecba1c943b3917c99d
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-27 23:02:52 +0100

    initial commit

---
+++ src/RadixSort.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Src;
+
+use Src\helper\Tree;
+
+class RadixSort
+{
+    /**
+     * @param array<int> $arr
+     * @return array<int>
+     */
+    public static function sort(array $arr): array
+    {
+        return $arr;
+    }
+}
