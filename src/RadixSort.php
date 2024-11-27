429e48df502910f5db24a1d0b247e2eed6ecc6ab
dimitrijjedich <dimitrij@startup-werk.de>
2024-11-27 23:24:43 +0100

    refactor collection logic

--- src/RadixSort.php
+++ src/RadixSort.php
@@ -19,9 +19,7 @@ class RadixSort
         }
         $arr = [];
         for($i = 0; $i < 10; $i++) {
-            if(!empty($partitions[$i])) {
-                $arr = array_merge($arr, $partitions[$i]);
-            }
+            $arr = array_merge($arr, $partitions[$i] ?? []);
         }
         return $arr;
     }
