e2fd9fea7cf1e43ba0fa59a23a8f5d2c8dfb8b01
dimitrijjedich <dimitrij@startup-werk.de>
2024-12-13 07:39:56 +0100

    formatting

--- src/helper/RedBlackNode.php
+++ src/helper/RedBlackNode.php
@@ -7,8 +7,8 @@ use Src\helper\enums\Color;
 class RedBlackNode
 {
     public function __construct(
-        private int $value,
-        private Color $color = Color::Red,
+        private int           $value,
+        private Color         $color = Color::Red,
         private ?RedBlackNode $left = null,
         private ?RedBlackNode $right = null
     ) {}
