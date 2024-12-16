1b504ab4447b13309b204811e049aa88bb74d7f1
dimitrijjedich <dimitrij@startup-werk.de>
2024-12-16 23:32:05 +0100

    remove usage of setParent from binaryTreeInsert

--- src/helper/RedBlackTree.php
+++ src/helper/RedBlackTree.php
@@ -81,14 +81,12 @@ class RedBlackTree
             if ($value < $pointer->getValue()) {
                 if ($pointer->getLeft() == null) {
                     $pointer->setLeft($node);
-                    $node->setParent($pointer);
                     return $node;
                 }
                 $pointer = $pointer->getLeft();
             } else {
                 if ($pointer->getRight() == null) {
                     $pointer->setRight($node);
-                    $node->setParent($pointer);
                     return $node;
                 }
                 $pointer = $pointer->getRight();
