41d9aa9eb50090eaec12c23b498d2c5a21756391
dimitrijjedich <dimitrij@startup-werk.de>
2024-12-21 18:04:47 +0100

    initial commit

---
+++ src/ShellSort.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Src;
+
+class ShellSort
+{
+    /**
+     * @param array<int,float> $arr
+     * @return array<int,float>
+     */
+    public static function sort(array $arr): array
+    {
+        return $arr;
+    }
+}
