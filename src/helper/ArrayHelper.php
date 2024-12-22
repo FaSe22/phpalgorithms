4f60c95856f747b8d969ab8f06c77d2f045ed989
dimitrijjedich <dimitrij@startup-werk.de>
2024-12-22 15:11:33 +0100

    add min and max functions using the php build in methods

--- src/helper/ArrayHelper.php
+++ src/helper/ArrayHelper.php
@@ -4,5 +4,25 @@ namespace Src\helper;
 
 class ArrayHelper
 {
+    /**
+     * Returns the smallest value of the array
+     *
+     * @param array<int, int|float> $array
+     * @return int|float
+     */
+    public static function getMin(array $array): int|float
+    {
+        return min($array);
+    }
 
+    /**
+     * Returns the biggest value of the array
+     *
+     * @param array<int, int|float> $array
+     * @return int|float
+     */
+    public static function getMax(array $array): int|float
+    {
+        return max($array);
+    }
 }
