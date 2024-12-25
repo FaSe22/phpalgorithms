93a1e8489d7ba5028aead122a76d864341465913
dimitrijjedich <dimitrij@startup-werk.de>
2024-12-25 20:39:43 +0100

    initial commit

---
+++ src/BucketSort.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Src;
+
+class BucketSort
+{
+    /**
+     * @param array<int, int|float> $arr
+     * @return array<int, int|float>
+     */
+    public static function sort(array $arr): array
+    {
+        return $arr;
+    }
+}
