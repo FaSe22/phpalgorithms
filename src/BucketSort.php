e211d5f64f6ab5fd7ec1ae3a67ececf685a70dbe
dimitrijjedich <dimitrij@startup-werk.de>
2024-12-26 16:09:01 +0100

    fix distribution into buckets

--- src/BucketSort.php
+++ src/BucketSort.php
@@ -11,10 +11,17 @@ class BucketSort
     public static function sort(array $arr): array
     {
         $length = count($arr);
+        $min = min($arr);
+        $max = max($arr);
+        if ($min == $max) {
+            return $arr;
+        }
+        $range = $max - $min;
         $bucketSize = (int)sqrt($length);
         $buckets = [];
         foreach ($arr as $element) {
-            $buckets[(int)$element%$bucketSize][] = $element;
+            $index = (($element - $min) / $range) * ($bucketSize - 1);
+            $buckets[(int)$index][] = $element;
         }
         return $arr;
     }
